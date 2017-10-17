8258a86082f6e781026058ca2034af22b9c88730
Marco Solazzi <marco.solazzi@gmail.com>
2017-10-17 20:58:22 +0200

    refactor(scaffold): refactoring

--- commands/scaffold.js
+++ commands/scaffold.js
@@ -11,7 +11,7 @@ const logger = require('../lib/logger');
 const generate = require('../lib/generator');
 
 if (semver.satisfies(process.version, pkg.engines.node) === false) {
-    logger.fatal(`Your current version of Node.js doesn't satisfy the minimun requirement: ${pkg.engines.node}`);
+    logger.fatal(`[scaffold] Your current version of Node.js doesn't satisfy the minimun requirement: ${pkg.engines.node}`);
     return;
 }
 
@@ -30,14 +30,14 @@ program
  */
 
 program.on('--help', () => {
-    console.log('  Examples:');
-    console.log();
-    console.log(chalk.gray('    # scaffold using template in ./scaffold/component/template'));
-    console.log('    $ umeboshi scaffold component');
-    console.log();
-    console.log(chalk.gray('    # scaffold using template in ./scaffold/react/stateless/template'));
-    console.log('    $ umeboshi scaffold react/stateless');
-    console.log();
+    logger.log('  Examples:');
+    logger.log();
+    logger.log(chalk.gray('    # scaffold using template in ./scaffold/component/template'));
+    logger.log('    $ umeboshi scaffold component');
+    logger.log();
+    logger.log(chalk.gray('    # scaffold using template in ./scaffold/react/stateless/template'));
+    logger.log('    $ umeboshi scaffold react/stateless');
+    logger.log();
 });
 
 program.parse(process.argv);
@@ -51,13 +51,13 @@ if (!program.verbose && Number.isFinite(program.logLevel)) {
     logger.setLevel(0);
 }
 
-logger.verbose(`Program started with arguments: ${program.args.join(', ')}`);
+logger.verbose(`[scaffold] Program started with arguments: ${program.args.join(', ')}`);
 
 const input = program.args[0];
 const [match, namespace, template = ''] = input.match(/^([^/]+)(?:\/([^/]+)|)$/) || [];
 
 if (!namespace) {
-    logger.fatal(`invalid namespace "${namespace}"`);
+    logger.fatal(`[scaffold] Invalid namespace "${namespace}"`);
     return;
 }
 
@@ -66,16 +66,16 @@ const folderList = [
     path.join(process.cwd(), 'node_modules', `umeboshi-scaffold-${namespace}`, template)
 ];
 
-logger.verbose(`Looking for template in: ${folderList.join(', ')}`);
+logger.verbose(`[scaffold] Looking for template in: ${folderList.join(', ')}`);
 
 const workingFolder = folderList.find(fs.existsSync);
 
 if (!workingFolder) {
-    logger.fatal(`Template "${input}" not found`);
+    logger.fatal(`[scaffold] Template "${input}" not found`);
     return;
 }
 
-logger.verbose(`Template "${input}" resolved as "${workingFolder}"`);
+logger.verbose(`[scaffold] Template "${input}" resolved as "${workingFolder}"`);
 
 try {
     const templateIndex = path.join(workingFolder, 'index.js');
@@ -84,10 +84,10 @@ try {
 
     const completed = (err) => {
         if (err) {
-            logger.fatal(`Scaffolding failed: ${err.message.trim()}`);
+            logger.fatal(`[scaffold] Scaffolding failed: ${err.message.trim()}`);
             return;
         }
-        logger.message('Scaffolding completed!\n');
+        logger.message('[scaffold] Scaffolding completed!\n');
     };
 
     inquirer
@@ -98,11 +98,11 @@ try {
                 return;
             }
             answers.to = path.resolve(process.cwd(), answers.to);
-            logger.verbose(`Running with options "${JSON.stringify(answers)}`);
+            logger.verbose(`[scaffold] Running with options "${JSON.stringify(answers)}`);
             generate(Object.assign({ command: 'scaffold' }, answers, {
                 src: workingFolder
             }), completed);
         });
 } catch (e) {
-    logger.fatal(e);
+    logger.fatal(`[scaffold] ${e.toString()}`);
 }
